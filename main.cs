4962c485f4d26184340455fe93d6c634e8499c04
TomGullen <tom@scirra.com>
2014-04-14 12:04:12 +0100

    Update main.cs
    OS detection added
    Added empty constructor that gets user agent

--- main.cs
+++ main.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Web;
 
 namespace v2.Tracker
 {
@@ -10,13 +11,32 @@ namespace v2.Tracker
         public string UserAgent { get; private set; }
         public Browser Browser { get; private set; }
         public Architecture Architecture { get; private set; }
+        public OperatingSystem OperatingSystem { get; private set; }
+        public SearchBot SearchBot { get; private set; }
+        public bool IsSearchBot { get; private set; }
 
+        public UserAgentInfo()
+        {
+            Load(HttpContext.Current.Request.UserAgent);
+        }
         public UserAgentInfo(string userAgent)
+        {
+            Load(userAgent);
+        }
+        private void Load(string userAgent)
         {
             UserAgent = userAgent;
             Browser = Parser.GetBrowser(userAgent);
             Architecture = Parser.GetArchitecture(userAgent);
+            OperatingSystem = Parser.GetOperatingSystem(userAgent);
+            SearchBot = Parser.GetSearchBot(userAgent);
+            IsSearchBot = SearchBot != SearchBot.NotSearchBot;
         }
+    }
+
+    public enum SearchBot
+    {
+        NotSearchBot = 0
     }
 
     public enum Browser
@@ -36,8 +56,84 @@ namespace v2.Tracker
         x32 = 2
     }
 
+    public enum OperatingSystem
+    {
+        Unknown = 0,
+        Windows3_1 = 1,
+        Windows95 = 2,
+        Windows98 = 3,
+        Windows2000 = 4,
+        WindowsXP = 5,
+        WindowsServer2003 = 6,
+        WindowsVista = 7,
+        Windows7 = 8,
+        Windows8 = 9,
+        WindowsNT4_0 = 10,
+        WindowsME = 11,
+        OpenBSD = 12,
+        SunOS = 13,
+        Linux = 14,
+        MacOS = 15,
+        QNX = 16,
+        BeOS = 17,
+        OS2 = 18
+    }
+
     public class Parser
     {
+        /// <summary>
+        /// Get corresponding system architecture for given user agent
+        /// http://stackoverflow.com/a/228267/356635
+        /// </summary>
+        public static OperatingSystem GetOperatingSystem(string userAgent)
+        {
+            if (userAgent.IndexOf("Windows 3.11", StringComparison.CurrentCultureIgnoreCase) != -1)
+                return OperatingSystem.Windows3_1;
+            if (userAgent.IndexOf("Windows 95", StringComparison.CurrentCultureIgnoreCase) != -1)
+                return OperatingSystem.Windows95;
+            if (userAgent.IndexOf("Windows 98", StringComparison.CurrentCultureIgnoreCase) != -1)
+                return OperatingSystem.Windows98;
+            if (userAgent.IndexOf("Windows 2000", StringComparison.CurrentCultureIgnoreCase) != -1)
+                return OperatingSystem.Windows2000;
+            if (userAgent.IndexOf("Windows XP", StringComparison.CurrentCultureIgnoreCase) != -1)
+                return OperatingSystem.WindowsXP;
+            if (userAgent.IndexOf("Windows Server 2003", StringComparison.CurrentCultureIgnoreCase) != -1)
+                return OperatingSystem.WindowsServer2003;
+            if (userAgent.IndexOf("Windows Vista", StringComparison.CurrentCultureIgnoreCase) != -1)
+                return OperatingSystem.WindowsVista;
+            if (userAgent.IndexOf("Windows 7", StringComparison.CurrentCultureIgnoreCase) != -1)
+                return OperatingSystem.Windows7;
+            if (userAgent.IndexOf("Windows 8", StringComparison.CurrentCultureIgnoreCase) != -1)
+                return OperatingSystem.Windows8;
+            if (userAgent.IndexOf("Windows NT 4.0", StringComparison.CurrentCultureIgnoreCase) != -1)
+                return OperatingSystem.WindowsNT4_0;
+            if (userAgent.IndexOf("Windows ME", StringComparison.CurrentCultureIgnoreCase) != -1)
+                return OperatingSystem.WindowsME;
+            if (userAgent.IndexOf("Open BSD", StringComparison.CurrentCultureIgnoreCase) != -1)
+                return OperatingSystem.OpenBSD;
+            if (userAgent.IndexOf("Sun OS", StringComparison.CurrentCultureIgnoreCase) != -1)
+                return OperatingSystem.SunOS;
+            if (userAgent.IndexOf("Linux", StringComparison.CurrentCultureIgnoreCase) != -1)
+                return OperatingSystem.Linux;
+            if (userAgent.IndexOf("Mac OS", StringComparison.CurrentCultureIgnoreCase) != -1)
+                return OperatingSystem.MacOS;
+            if (userAgent.IndexOf("QNX", StringComparison.CurrentCultureIgnoreCase) != -1)
+                return OperatingSystem.QNX;
+            if (userAgent.IndexOf("BeOS", StringComparison.CurrentCultureIgnoreCase) != -1)
+                return OperatingSystem.BeOS;
+            if (userAgent.IndexOf("OS/2", StringComparison.CurrentCultureIgnoreCase) != -1)
+                return OperatingSystem.OS2;
+            return OperatingSystem.Unknown;
+        }
+
+        /// <summary>
+        /// Get the corresponding search bot for given user agent
+        /// </summary>
+        public static SearchBot GetSearchBot(string userAgent)
+        {
+            return SearchBot.NotSearchBot;
+        }
+
         /// <summary>
         /// Get corresponding system architecture for given user agent
         /// http://stackoverflow.com/a/13709431/356635
